7464de15048600b53dd502f2452141e2f84e6c1e
twytwy <weiyang_tan@mymail.sutd.edu.sg>
2014-11-01 16:00:27 +0800

    SQL schema v3
    modified parameters

--- db/BookstoreSchema.sql
+++ db/BookstoreSchema.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE IF NOT EXISTS Bookstore;
+--CREATE DATABASE IF NOT EXISTS Bookstore;
 
-USE Bookstore;
+--USE Bookstore;
 
 CREATE TABLE Books (
 	ISBN CHAR(15) PRIMARY KEY,
-	title CHAR(40),
-	authors	CHAR(40),
-	publisher CHAR(40),
+	title CHAR(140),
+	authors	CHAR(140),
+	publisher CHAR(140),
 	year_of_pub DATE,
 	copies_avail INTEGER,
 	price REAL,
-	format CHAR(9) CHECK(format="hardcover" or format="softcover"),
-	keywords CHAR(40),
-	subject CHAR(40)
+	format CHAR(9), --CHECK(format="hardcover" or format="softcover"),
+	keywords CHAR(140),
+	subject CHAR(140)
 );
 
 CREATE TABLE Customers (
 	login CHAR(20) PRIMARY KEY,
 	password CHAR(20),
-	name CHAR(40),
+	name CHAR(100),
 	credit_card_no CHAR(20),
-	address CHAR(50),
+	address CHAR(140),
 	phone BIGINT
 );
 
@@ -30,7 +30,7 @@ CREATE TABLE Orders (
 	order_id BIGINT,
 	copies INTEGER,
 	date DATE,
-	status CHAR(10) CHECK(status="Confirmed" or status="Shipping" or status = "Pending" or status="Delivered"),
+	status CHAR(10), --CHECK(status="Confirmed" or status="Shipping" or status = "Pending" or status="Delivered"),
 	PRIMARY KEY (user_id, book_id, order_id),
 	FOREIGN KEY (user_id) REFERENCES Customers(login),
 	FOREIGN KEY (book_id) REFERENCES Books(ISBN)
@@ -42,9 +42,9 @@ CREATE TABLE Feedbacks (
 	score INTEGER,
 	comment CHAR(140),
 	date DATE,
-	useless INTEGER,
-	useful INTEGER,
-	very_useful INTEGER,
+	useless INTEGER, --DEFAULT 0,
+	useful INTEGER, --DEFAULT 0,
+	very_useful INTEGER, --DEFAULT 0,
 	PRIMARY KEY (book_id, user_id),
 	FOREIGN KEY (book_id) REFERENCES Books(ISBN),
 	FOREIGN KEY (user_id) REFERENCES Customers(login)
@@ -54,7 +54,7 @@ CREATE TABLE Likes (
 	book_id CHAR(15),
 	commenter_id CHAR(20),
 	liker_id CHAR(20),
-	rating INTEGER,
+	rating INTEGER, --CHECK(rating=0 or rating=1 or rating=2),
 	PRIMARY KEY (book_id, commenter_id, liker_id),
 	FOREIGN KEY (book_id, commenter_id) REFERENCES Feedbacks,
 	FOREIGN KEY (liker_id) REFERENCES Customers(login)
