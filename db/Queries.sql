8c49b8ceb5bdefb5f3a9d3549b92a077d21d9944
twytwy <weiyang_tan@mymail.sutd.edu.sg>
2014-11-04 10:33:01 +0800

    SQL queries qns3 completed

--- db/Queries.sql
+++ db/Queries.sql
@@ -9,4 +9,36 @@
 	FROM Orders;
 	-- after this just need to insert into table
 
--- 3. TBC
+-- 3. checking user record. let user be "user3"
+	-- account info
+	SELECT name, credit_card_no, address, phone
+	FROM Customers
+	WHERE login = 'user3';
+
+	-- history of orders
+	SELECT order_id, book_id, copies, date, status
+	FROM Orders
+	WHERE user_id = 'user3';
+
+	-- history of feedbacks
+	SELECT book_id, score, comment, date, (useful + (very_useful*2.0)) AS sum_rates, (useless + useful +very_useful) AS num_rates
+	FROM Feedbacks
+	WHERE user_id = 'user3';
+
+	-- history of liked feedbacks
+	SELECT f.book_id, f.user_id, f.score, f.comment, f.date, (f.useful + (f.very_useful*2.0)) AS sum_rates, (f.useless + f.useful + f.very_useful) AS num_rates, l.rating
+	FROM Feedbacks f, (SELECT book_id, commenter_id, rating
+					   FROM Likes
+					   WHERE liker_id = 'mail') l
+	WHERE f.book_id = l.book_id
+	AND f.user_id = l.commenter_id
+	AND EXISTS (SELECT book_id, commenter_id
+				FROM Likes
+				WHERE liker_id = 'mail'
+				AND book_id = f.book_id
+				AND commenter_id = f.user_id);
+
+-- 4. TBC
+
+-- SELECT * FROM Feedbacks;
+-- SELECT * FROM Likes;
